d597a8636a46dc502d38b30d6fe3c37f9e5e7b44
Subhani Shaik <99859528+Sksubhan@users.noreply.github.com>
2025-02-28 11:46:51 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from outliers_Detection import Outliers_detection
-from Data_ingestion import sensor_data_Ingestion,threshold_data_Ingestion
+from Data_ingestion import data_Ingestion
 from Reporting import reporting_data
 from src.errors_log import log_error
 
-sensor_data=input('Please Enter the sensor_data_set name [with .csv]:')
-thresholds_data=input('Please Enter the thresholds_data_set name [with .csv]:')
+sensor_data=input('Please Enter the sensor_data_set name with extension:')
+thresholds_data=input('Please Enter the thresholds_data_set name with extension:')
 try:
-    print(Outliers_detection(sensor_data_Ingestion(sensor_data),threshold_data_Ingestion(thresholds_data)))
+    print(Outliers_detection(data_Ingestion(sensor_data),data_Ingestion(thresholds_data)))
     print(reporting_data(sensor_data,thresholds_data))
 except Exception as e:
-    log_error('Please give proper data_set name with .csv at the end',e)
+    log_error('ERROR002: please give proper name with extension ',e)

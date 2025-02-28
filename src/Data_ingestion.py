feda6be3e825efe934cf1398bd02b83d2b8c302a
Subhani Shaik <99859528+Sksubhan@users.noreply.github.com>
2025-02-28 11:42:37 +0530

    Update Data_ingestion.py

--- src/Data_ingestion.py
+++ src/Data_ingestion.py
@@ -1,29 +1,29 @@
 from errors_log import log_error
 import pandas as pd
-def sensor_data_Ingestion(data):
+from pathlib import Path as path
+def data_Ingestion(data):
+
+    # read any format data and convert into DataFrame
     try:
-        sensor_data=pd.read_csv(data)
-        return sensor_data
+        if path(data).suffix == '.csv':
+            df = pd.read_csv(data)
+        elif path(data).suffix == '.json':
+            df = pd.read_json(data)
+        elif path(data).suffix in ['.xml', '.xlsx']:
+            df = pd.read_excel(data)
+        return df
     except FileNotFoundError as e:
-        log_error(f'Unable to fetch the file ',e)
+        log_error(f'ERROR001 ',e)
         exit(1)
     except pd.errors.ParserError as e:
-        log_error("Invalid data format",e)
+        log_error("ERROR",e)
         exit(1)
 
-def threshold_data_Ingestion(data):
-    try:
-        threshold_data=pd.read_csv(data)
-        return threshold_data
-    except FileNotFoundError as e:
-        log_error(f'Unable to fetch the file ', e)
-        exit(1)
-    except pd.errors.ParserError as e:
-        log_error("Invalid data format", e)
-        exit(1)
 
 
 if __name__=='__main__':
-    sensor_data_Ingestion('sensor_data.csv')
-    threshold_data_Ingestion('thresholds.csv')
+    sensor_data=data_Ingestion('sensor_data.csv')
+    threshold_data=data_Ingestion('thresholds.csv')
+
+
 
